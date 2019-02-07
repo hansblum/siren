e598827d36dd3fb0df049f28ca045c9df8bda183
alina achkasova <alina.achkasova@gmail.com>
2019-02-08 00:31:37 +0100

    Adding redirect to the second tab from the first one

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+  openForm() {
+      let currentLocation = new String(window.location);
+      let newLocation = currentLocation.replace('tab1', 'tab2');
+      window.location.href = newLocation;
+  }
+}
