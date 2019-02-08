e1a81c5c581f3accc0e646aee4d539c95adf2483
Johayra Besselse <johayrabesselse@MacBook-Air-van-Johayra.local>
2019-02-08 11:43:28 +0100

    added perrito to get geolocation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { BackgroundGeolocation, BackgroundGeolocationConfig, BackgroundGeolocationResponse } from '@ionic-native/background-geolocation';
-
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { Platform } from '@ionic/angular';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -18,36 +17,11 @@ import { BackgroundGeolocation, BackgroundGeolocationConfig, BackgroundGeolocati
   providers: [
     StatusBar,
     SplashScreen,
+    Geolocation,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor(private backgroundGeolocation: BackgroundGeolocation, location: string) {
-   }
-}
-
-
-// Geolocation area
-const config: BackgroundGeolocationConfig = {
-  desiredAccuracy: 10,
-  stationaryRadius: 20,
-  distanceFilter: 30,
-  debug: true, //  enable this hear sounds for background-geolocation life-cycle.
-  stopOnTerminate: false, // enable this to clear background location settings when the app terminates
-};
-
-this.backgroundGeolocation.configure(config)
-  .subscribe((location: BackgroundGeolocationResponse) => {
-    this.location = location;
-    console.log('puppie', location);
-
-    // IMPORTANT:  You must execute the finish method here to inform the native plugin that you're finished,
-    // and the background-task may be completed.  You must do this regardless if your HTTP request is successful or not.
-    // IF YOU DON'T, ios will CRASH YOUR APP for spending too much time in the background.
-    this.backgroundGeolocation.finish(); // FOR IOS ONLY
-
-  });
-
-// start recording location
-this.backgroundGeolocation.start();
+  
+  }
