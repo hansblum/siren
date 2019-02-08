45ef6e2067da66be48ad1fa28bac99c435f2a3da
Juan Besselse <C45506@launcher.int.abnamro.com>
2019-02-08 10:04:57 +0100

    Added geolocation plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BackgroundGeolocation, BackgroundGeolocationConfig, BackgroundGeolocationResponse } from '@ionic-native/background-geolocation';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -20,4 +22,32 @@ import { AppComponent } from './app.component';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private backgroundGeolocation: BackgroundGeolocation, location: string) {
+   }
+}
+
+
+// Geolocation area
+const config: BackgroundGeolocationConfig = {
+  desiredAccuracy: 10,
+  stationaryRadius: 20,
+  distanceFilter: 30,
+  debug: true, //  enable this hear sounds for background-geolocation life-cycle.
+  stopOnTerminate: false, // enable this to clear background location settings when the app terminates
+};
+
+this.backgroundGeolocation.configure(config)
+  .subscribe((location: BackgroundGeolocationResponse) => {
+    this.location = location;
+    console.log('puppie', location);
+
+    // IMPORTANT:  You must execute the finish method here to inform the native plugin that you're finished,
+    // and the background-task may be completed.  You must do this regardless if your HTTP request is successful or not.
+    // IF YOU DON'T, ios will CRASH YOUR APP for spending too much time in the background.
+    this.backgroundGeolocation.finish(); // FOR IOS ONLY
+
+  });
+
+// start recording location
+this.backgroundGeolocation.start();
