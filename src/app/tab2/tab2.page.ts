1e9f4d182600af588771b3ca2eb3e88152a97a76
alina achkasova <alina.achkasova@gmail.com>
2019-02-08 01:04:03 +0100

    Adding new fields after clicking on 'Add new contact person' button

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -5,4 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+  addContactPerson() {
+    let dummy = '<ion-item><ion-label position="floating">Name</ion-label><ion-input></ion-input></ion-item><ion-item><ion-label position="floating">Phone number</ion-label><ion-input></ion-input></ion-item>\r\n';
+    document.getElementsByClassName('contact-person')[0].innerHTML += dummy;    
+  }
+}
