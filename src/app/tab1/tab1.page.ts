0f83e0818f7b87a6d25bb9ee5721b9e09464b997
Hans Blum <jhlblum@gmail.com>
2019-02-09 09:49:35 +0100

    Formatting only

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -15,10 +15,10 @@ export class Tab1Page {
   constructor(private geolocation: Geolocation, private sms: SMS) {
   }
 
-  sendMessage(){
+  sendMessage() {
     this.geolocation.getCurrentPosition().then((resp) => {
-      this.myLocation = resp.coords;
-      this.sms.send('0612221161',
+    this.myLocation = resp.coords;
+    this.sms.send('0612221161',
       `I am in danger!  😱 , help me please [20:43, 8-2-2019]
        Alina: https://maps.google.com/?q=${resp.coords.latitude},${resp.coords.longitude}`);
     }).catch((error) => {
@@ -27,14 +27,14 @@ export class Tab1Page {
     });
 
     this.geolocation.watchPosition().subscribe((data) => {
-     // data can be a set of coordinates, or an error (if an error occurred).
-     // data.coords.latitude
-     // data.coords.longitude
+      // data can be a set of coordinates, or an error (if an error occurred).
+      // data.coords.latitude
+      // data.coords.longitude
     });
   }
   openForm() {
-      let currentLocation = new String(window.location);
-      let newLocation = currentLocation.replace('tab1', 'tab2');
-      window.location.href = newLocation;
-  }  
+    let currentLocation = new String(window.location);
+    let newLocation = currentLocation.replace('tab1', 'tab2');
+    window.location.href = newLocation;
+  }
 }
